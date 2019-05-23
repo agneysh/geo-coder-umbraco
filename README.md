03d31b3a2c7c8f7eab1dfd25fb7246e542a2880b
Agneysh <Agneysh96@gmail.com>
2019-05-23 14:44:10 +0400

    feat: add code

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# geo-coder-umbraco
-Geo coding plugin for Umbraco
+# Geo-Coder-Umbraco
+Geo coding plugin for Umbraco. Feel free to fork and contribute.
+
+# Contributors
+
+[agneysh](https://github.com/agneysh)
+
+
+# Installation
+
+Download / Clone the project and put it inside a folder under
+
+App_Plugins
+
+geoCoder ( create the folder )
+
+(then you have the files here)
+
+# Technical Details
+
+The GeoCoder will use the address given on the text field and generate coordinates from it.
+
+**Note:**
+**Add your google api key on the line 7: "assetsService.loadJs("https://maps.googleapis.com/maps/api/js?key="); "**
+
+
+
+Tested up to: Umbraco 7.2
+
+
+# Commit Messages
+Must be one of the following
+
+* build: Changes that affect the build system or external dependencies (example scopes: webpack, npm)
+* config: configuration changes only
+* docs: Documentation only changes
+* feat: A new feature
+* fix: A bug fix
+* perf: A code change that improves performance
+* refactor: A code change that neither fixes a bug nor adds a feature
+* style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
---
+++ googlemapgeocoder.controller.js
@@ -0,0 +1,48 @@
+﻿angular.module("umbraco").controller("googlemapgeocoder.controller",
+    function ($scope, assetsService) {
+        // initialise variables
+        var geocoder;
+
+        // use assests service to load Google maps api
+        assetsService.loadJs("https://maps.googleapis.com/maps/api/js?key=");
+
+        // called when Google maps api has loaded
+        function initialize() {
+            // create geocoder
+            geocoder = new google.maps.Geocoder();
+
+        }
+
+        // runs when geocode button in view is clicked
+        $scope.codeAddress = function () {
+            var address = $scope.model.value.address;
+
+            var lat, lng;
+            // use Google api to geocode location
+
+            initialize();
+            geocoder.geocode({ 'address': address }, function (results, status) {
+                // set location if geocode successful
+                lat = results[0].geometry.location.lat();
+                lng = results[0].geometry.location.lng();
+                $('#clicker').click();
+
+                if (status == google.maps.GeocoderStatus.OK) {
+                    // set something to $scope.model.value here.
+
+                    $scope.model.value.lat = lat;
+                    $scope.model.value.lng = lng;
+                    console.log($scope.model)
+                    var unsubscribe = $scope.$on("formSubmitting", function (ev, args) {
+                        $scope.model.value = { 'address': address, 'lat':lat, 'lng':lng};
+                        //$scope.model.latitude = lat;
+                        //$scope.model.longitude = lng;
+                    });
+
+                } else {
+                    alert('Geocode was not successful');
+
+                }
+            });
+        }
+    }); 
